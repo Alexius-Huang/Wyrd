947ac30daa93777e4f45652f826ea4a255ad2510
Maxwell-Alexius <alexius.huang@gmail.com>
2020-01-23 15:16:47 +0800

    Refactor the parseConditionalExpr function

--- src/parser/error.ts
+++ src/parser/error.ts
@@ -1,3 +1,7 @@
 export function ParserError(msg: string): never {
   throw new Error(`Parser: ${msg}`);
 }
+
+export function ParserErrorIf(bool: boolean, msg: string): void {
+  if (bool) ParserError(msg);
+}
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -10,7 +10,7 @@ import { parseFunctionDeclaration } from './function';
 import { parseAssignmentExpr } from './assignment';
 import { parsePrioritizedExpr } from './prioritized';
 import { parseBinaryOpExpr } from './operation';
-import { ParserError } from './error';
+import { ParserError, ParserErrorIf } from './error';
 import { BuiltinBinaryOperators } from './constants';
 
 export function parse(tokens: Array<T.Token>): T.AST {
@@ -112,69 +112,66 @@ export function parse(tokens: Array<T.Token>): T.AST {
   }
 
   function parseConditionalExpr(prevExpr?: T.Expr): T.Expr {
-    nextToken(); // Skip 'if' | 'elif' keyword
+    curTok = nextToken(); // Skip 'if' | 'elif' keyword
     let result: T.ConditionalExpr = { type: 'ConditionalExpr' };
 
     while (curTok.type !== 'arrow' && curTok.value !== 'then') {
       result.condition = parseExpr(result, { target: 'condition' });
-      nextToken();
-
-      if (curTok.type === 'newline')
-        ParserError('Expect condition to end followed by arrow `=>` or the `then` keyword');
+      curTok = nextToken();
+      ParserErrorIf(curTok.type === 'newline', 'Expect condition to end followed by arrow `=>` or the `then` keyword');
     }
 
     if (curTok.value === 'then') {
-      nextToken(); // Skip 'then' keyword
-      if (curTok.type as string !== 'newline')
-        ParserError('Expect no tokens after `then` keyword');
+      curTok = nextToken(); // Skip 'then' keyword      
+      ParserErrorIf(curTok.type !== 'newline', 'Expect no tokens after `then` keyword');
     }
 
-    nextToken(); // Skip '=>' inline-control operator or skip `newline` if using `then block`
+    curTok = nextToken(); // Skip '=>' inline-control operator or skip `newline` if using `then block`
 
-    while (curTok.type as string !== 'newline') {
+    while (curTok.type !== 'newline') {
       result.expr1 = parseExpr(result, { target: 'expr1' });
-      nextToken();
+      curTok = nextToken();
     }
 
-    nextToken(); // Skip newline
+    curTok = nextToken(); // Skip newline
 
     /* Handle elif is exactly the same as the if expression */
-    if (curTok.type as string === 'keyword' && curTok.value as string === 'elif') {
+    if (curTok.type === 'keyword' && curTok.value === 'elif') {
       result.expr2 = parseConditionalExpr();
       return result;
     }
 
     /* Handle else expression */
-    if (curTok.type as string === 'keyword' && curTok.value as string === 'else') {
-      nextToken(); // Skip 'else' keyword
+    if (curTok.type === 'keyword' && curTok.value === 'else') {
+      curTok = nextToken(); // Skip 'else' keyword
 
-      if (curTok.type as string !== 'arrow' && curTok.value as string !== 'then')
-        ParserError('Expect else condition to followed by arrow `=>` or the `then` keyword');
+      ParserErrorIf(
+        curTok.type !== 'arrow' && curTok.value !== 'then',
+        'Expect else condition to followed by arrow `=>` or the `then` keyword'
+      );
 
-      if (curTok.type as string === 'arrow') {
-        nextToken(); // Skip 'arrow' keyword
+      if (curTok.type === 'arrow') {
+        curTok = nextToken(); // Skip 'arrow' keyword
 
-        while (curTok.type as string !== 'newline') {
+        while (curTok.type !== 'newline') {
           result.expr2 = parseExpr(result, { target: 'expr2' });
-          nextToken();
+          curTok = nextToken();
         }
-      } else if (curTok.value as string === 'then') {
-        nextToken(); // Skip 'then' keyword
-        if (curTok.type as string === 'newline') {
-          nextToken(); // Skip 'newline' token
+      } else if (curTok.value === 'then') {
+        curTok = nextToken(); // Skip 'then' keyword
+        if (curTok.type === 'newline') {
+          curTok = nextToken(); // Skip 'newline' token
 
-          while (curTok.type as string !== 'newline') {
+          while (curTok.type !== 'newline') {
             result.expr2 = parseExpr(result, { target: 'expr2' });
-            nextToken();
+            curTok = nextToken();
           }
 
-          nextToken(); // Skip 'newline' token
-          if (curTok.value as string !== 'end')
-            ParserError('Expect `else then` expression to followed by an `end` keyword');
+          curTok = nextToken(); // Skip 'newline' token
+          ParserErrorIf(curTok.value !== 'end', 'Expect `else then` expression to followed by an `end` keyword');
 
-          nextToken(); // Skip 'end' token
-          if (curTok.type as string !== 'newline')
-            ParserError('Expect no tokens after `end` keyword');
+          curTok = nextToken(); // Skip 'end' token
+          ParserErrorIf(curTok.type !== 'newline', 'Expect no tokens after `end` keyword');
         }
       }
     }
