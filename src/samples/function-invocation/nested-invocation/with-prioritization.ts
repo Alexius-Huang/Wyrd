2b193a5a288be37afbbaaa5071c1222ba05a4267
Maxwell-Alexius <alexius.huang@gmail.com>
2020-01-30 15:19:01 +0800

    Add more test case for prioritized and params nested with parentheses function invocation expression

--- src/samples/function-invocation/nested-invocation/with-prioritization.ts
+++ src/samples/function-invocation/nested-invocation/with-prioritization.ts
@@ -4,7 +4,9 @@ import { createFunctionPatterns } from '../../helper';
 const program = `\
 funcA 1, 2, (funcB 3, 4), 5
 funcC 1 + 2 * (funcD 3, 4), 5
-`;
+`;`
+funcE 1 + 2 * (funcF 3, 4) + 5, 6
+`
 
 const tokens: Array<Token> = [
   { type: 'ident', value: 'funcA' },
@@ -36,6 +38,23 @@ const tokens: Array<Token> = [
   { type: 'comma', value: ',' },
   { type: 'number', value: '5' },
   { type: 'newline', value: '\n' },
+
+  // { type: 'ident', value: 'funcE' },
+  // { type: 'number', value: '1' },
+  // { type: 'plus', value: '+' },
+  // { type: 'number', value: '2' },
+  // { type: 'asterisk', value: '*' },
+  // { type: 'lparen', value: '(' },
+  // { type: 'ident', value: 'funcF' },
+  // { type: 'number', value: '3' },
+  // { type: 'comma', value: ',' },
+  // { type: 'number', value: '4' },
+  // { type: 'rparen', value: ')' },
+  // { type: 'plus', value: '+' },
+  // { type: 'number', value: '5' },
+  // { type: 'comma', value: ',' },
+  // { type: 'number', value: '6' },
+  // { type: 'newline', value: '\n' },
 ];
 
 const ast: AST = [
@@ -87,12 +106,49 @@ const ast: AST = [
     ],
     returnType: 'Num',
   },
+  // {
+  //   type: 'FunctionInvokeExpr',
+  //   name: 'funcE',
+  //   params: [
+  //     {
+  //       type: 'BinaryOpExpr',
+  //       operator: Op.Plus,
+  //       returnType: 'Num',
+  //       expr1: {
+  //         type: 'BinaryOpExpr',
+  //         operator: Op.Plus,
+  //         returnType: 'Num',
+  //         expr1: { type: 'NumberLiteral', value: '1', returnType: 'Num' },
+  //         expr2: {
+  //           type: 'BinaryOpExpr',
+  //           operator: Op.Asterisk,
+  //           returnType: 'Num',
+  //           expr1: { type: 'NumberLiteral', value: '2', returnType: 'Num' },
+  //           expr2: {
+  //             type: 'FunctionInvokeExpr',
+  //             name: 'funcF',
+  //             params: [
+  //               { type: 'NumberLiteral', value: '3', returnType: 'Num' },
+  //               { type: 'NumberLiteral', value: '4', returnType: 'Num' },
+  //             ],
+  //             returnType: 'Num',
+  //           },
+  //         },
+  //       },
+  //       expr2: { type: 'NumberLiteral', value: '5', returnType: 'Num' },  
+  //     },
+  //     { type: 'NumberLiteral', value: '6', returnType: 'Num' },
+  //   ],
+  //   returnType: 'Num',
+  // },
 ];
 
 const compiled = `\
 funcA(1, 2, funcB(3, 4), 5);
 funcC(1 + (2 * funcD(3, 4)), 5);
-`;
+`;`
+funcE(1 + (2 * funcF(3, 4)) + 5, 6);
+`
 
 const parseOptions: ParseOptions = {
   functions: createFunctionPatterns([
@@ -100,6 +156,8 @@ const parseOptions: ParseOptions = {
     ['funcB', [['Num.Num', 'Num']]],
     ['funcC', [['Num.Num', 'Num']]],
     ['funcD', [['Num.Num', 'Num']]],
+    ['funcE', [['Num.Num', 'Num']]],
+    ['funcF', [['Num.Num', 'Num']]],
   ]),
 };
 
