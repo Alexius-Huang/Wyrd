11568f265fcb56c6ac19037ce571c66319ab099d
Maxwell-Alexius <alexius.huang@gmail.com>
2020-01-31 14:33:54 +0800

    Throws error when list contain mixed type of values

--- src/test/builtin-types.spec.ts
+++ src/test/builtin-types.spec.ts
@@ -1,4 +1,5 @@
 import { FundamentalCompileTest } from './helper';
+import { compile } from '../index';
 
 describe('Builtin Types', () => {
   describe('Primitives', () => {
@@ -8,6 +9,12 @@ describe('Builtin Types', () => {
   describe('Composite Data Structure', () => {
     describe('List', () => {
       FundamentalCompileTest('builtin-types/list');
+
+      it('cannot contain mixed type of elements', () => {
+        const program = `\n[1 2 "3" 4 5]`;
+        const errorMsg = 'Parser: Expect List to contain of type `Num`, instead mixed with type `Str`';
+        expect(() => compile(program)).toThrowError(errorMsg);
+      });
     });
   });
 });
