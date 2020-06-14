9ac5c8166761d0a1a620a4b217940349e29b4f56
Maxwell-Alexius <alexius.huang@gmail.com>
2020-06-14 15:15:58 +0800

    Support maybe types value in record declaration

--- src/parser/record/literal.ts
+++ src/parser/record/literal.ts
@@ -57,7 +57,7 @@ export function parseRecordLiteral(
       }
     }
 
-    if (propValueExpr.return.isNotEqualTo(propValue.type))
+    if (propValueExpr.return.isNotAssignableTo(propValue.type))
       ParserError(`Expect property \`${propName}\` in record \`${recordDef.name}\` to receive value of type \`${propDef.type}\`, instead got value of type \`${propValueExpr.return}\``);
 
     propValue.value = propValueExpr;
@@ -68,8 +68,25 @@ export function parseRecordLiteral(
   }
 
   if (requiredPropertySet.size !== 0) {
-    const formattedStr = Array.from(requiredPropertySet).map(v => `\`${v}\``).join(', ');
-    ParserError(`Property of record \`${recordName}\` is missing: ${formattedStr}`);
+    const propArr = Array.from(requiredPropertySet);
+    for (let i = 0; i < propArr.length; i += 1) {
+      const propName = propArr[i];
+      const propDef = recordDef.getProperty(propName);
+      if (propDef.type.nullable) {
+        result.properties.push({
+          name: propName,
+          type: propDef.type,
+          value: { type: 'NullLiteral', return: DT.Null, value: 'Null' },
+        });
+
+        requiredPropertySet.delete(propName);
+      }
+    }
+
+    if (requiredPropertySet.size !== 0) {
+      const formattedStr = Array.from(requiredPropertySet).map(v => `\`${v}\``).join(', ');
+      ParserError(`Property of record \`${recordName}\` is missing: ${formattedStr}`);  
+    }
   }
 
   return result;
