01a4d15fe2b05e4226402820510f38b20f166860
Maxwell-Alexius <alexius.huang@gmail.com>
2020-06-19 18:01:47 +0800

    Resolve issue of record value assignment

--- src/parser/assignment/index.ts
+++ src/parser/assignment/index.ts
@@ -1,3 +1,4 @@
 export { parseConstantDeclaration } from './constant-declaration';
 export { parseVarAssignmentExpr } from './var-assignment';
 export { parseVarDeclaration } from './var-declaration';
+export { parseRecordValueAssignmentExpr } from './record-value-assignment';
